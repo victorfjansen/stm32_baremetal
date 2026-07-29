6b17517e55fabbed4e6cc6c1cdf3da5111bf1c93
Victor Jansen <victorfjansen@gmail.com>
2026-07-29 18:43:16 -0300

    baremetal: creating modules for uart transmiter driver

---
+++ 7_uart_modular/Inc/uart.h
@@ -0,0 +1,15 @@
+/*
+ * uart.h
+ *
+ *  Created on: Jul 29, 2026
+ *      Author: victo
+ */
+
+#ifndef UART_H_
+#define UART_H_
+
+#include <stdint.h>
+#include "stm32f4xx.h"
+void uart2_tx_init(void);
+
+#endif /* UART_H_ */
