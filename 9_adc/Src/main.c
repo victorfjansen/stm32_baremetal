902713b222175fa31cfadd8b11412801eb70054b
Victor Jansen <victorfjansen@gmail.com>
2026-07-30 17:46:07 -0300

    baremetal: adc converter driver

---
+++ 9_adc/Src/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdint.h>
+#include "uart.h"
+#include "adc.h"
+
+uint32_t sensor_value;
+
+int main(void) {
+
+
+	uart2_tx_init();
+	pa1_adc_init();
+	start_conversion();
+
+	while(1) {
+		sensor_value = adc_read();
+		printf("Sensor value: %d  ", (int)sensor_value);
+	}
+
+}
+
+
